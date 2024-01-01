88e13932e4214bc8448d01a000e9c1ef1e315773
Ritika Sharma <ritikasharma11154@gmail.com>
2024-01-02 01:45:24 +0530

    Added Project

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use("/tour", tourRouter);
 app.get("/", (req, res) => {
   res.send("Welcome to tour API");
 });
-server.use(express.static(path.resolve(__dirname, "client/build")));
+server.use(express.static(path.resolve(__dirname, "./client/build")));
 server.use("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
